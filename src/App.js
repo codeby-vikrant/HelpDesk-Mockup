ec608e0ea8a4434da400b8df8ce668c6109dfdeb
codeby-vikrant <workwithvikrant0111@gmail.com>
2025-07-04 21:55:28 +0530

    Added routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignInPage from "./screens/sign_in_page";
+import SignUpPage from "./screens/sign_up_page";
+import UserDashboardPage from "./screens/user_dashboard_page";
+import NewTicketPage from "./screens/new_ticket_page";
 
-function App() {
-  return <SignInPage />;
-}
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/dashboard" element={<UserDashboardPage />} />
+        <Route path="/new-ticket" element={<NewTicketPage />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
