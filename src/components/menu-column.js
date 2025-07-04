ad64e5ab527073400423a9aa903b26c095ffeda6
codeby-vikrant <workwithvikrant0111@gmail.com>
2025-07-04 21:57:05 +0530

    created separate component for menu column

---
+++ src/components/menu-column.js
@@ -0,0 +1,29 @@
+import { MdDashboard } from "react-icons/md";
+import { FaTicket, FaTicketSimple } from "react-icons/fa6";
+import { Link } from "react-router-dom";
+import "../styles/menu-column.css";
+
+const MenuColumn = () => {
+  return (
+    <aside className="menu-column">
+      <div className="menu-option">
+        <MdDashboard className="menu-icon" />
+        <Link to="/dashboard" className="menu-option-link">
+          <span>Dashboard</span>
+        </Link>
+      </div>
+      <div className="menu-option">
+        <FaTicket className="menu-icon" />
+        <Link to="/new-ticket" className="menu-option-link">
+          <span>New Ticket</span>
+        </Link>
+      </div>
+      <div className="menu-option">
+        <FaTicketSimple className="menu-icon" />
+        <span>My Ticket</span>
+      </div>
+    </aside>
+  );
+};
+
+export default MenuColumn;
