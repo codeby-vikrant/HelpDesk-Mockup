bda7da4216f76c82658a405f68bacbfbc30a358d
codeby-vikrant <workwithvikrant0111@gmail.com>
2025-07-05 09:35:35 +0530

    created a list of users

--- src/App.js
+++ src/App.js
@@ -4,6 +4,22 @@ import SignInPage from "./screens/sign_in_page";
 import SignUpPage from "./screens/sign_up_page";
 import UserDashboardPage from "./screens/user_dashboard_page";
 import NewTicketPage from "./screens/new_ticket_page";
+import { useState } from "react";
+
+const users = [
+  {
+    username: "user", password: "user123", role:"customer"
+  },
+  {
+    username: "operator", password: "operator123", role: "operator"
+  },
+  {
+    username: "technician", password: "technician123", role: "technician"
+  },
+  {
+    username: "admin", password: "admin123", role: "admin"
+  }
+]
 
 const App = () => {
   return (
